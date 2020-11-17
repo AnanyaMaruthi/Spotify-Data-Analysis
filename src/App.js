c2d8c53b141edd0d477a5c9cc605754335218858
AnanyaMaruthi <ananyasheshu@gmail.com>
2020-11-17 19:32:03 +0530

    Refactored code

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./styles/theme";
 import Analytics from "./components/analytics";
 import DataDescription from "./components/dataDescription";
-import Header from "./components/header";
 
 const { default: Cover } = require("./components/cover");
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div>
-        {/* <Header /> */}
-        <Cover />
-        <Analytics />
-        <DataDescription />
-      </div>
+      <Cover />
+      <DataDescription />
+      <Analytics />
     </ThemeProvider>
   );
 }
